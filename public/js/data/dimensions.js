5af13aab624278c973cd5630124bece867f7db8a
Ann Yuan <ayuan@thisisgrow.com>
2015-08-05 06:39:06 -0400

    add some data

---
+++ public/js/data/dimensions.js
@@ -0,0 +1,14 @@
+module.exports = [
+  {
+    name: 'Importance',
+    adjective: 'important'
+  },
+  {
+    name: 'Tractability',
+    adjective: 'tractable'
+  },
+  {
+    name: 'Neglectedness',
+    adjective: 'neglected'
+  }
+];
