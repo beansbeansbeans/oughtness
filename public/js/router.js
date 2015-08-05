8c32601debe5f79ec1179bf4fb04690e2f6bf753
Ann Yuan <ayuan@thisisgrow.com>
2015-08-05 07:44:59 -0400

    basic redirect functionality

--- public/js/router.js
+++ public/js/router.js
@@ -1,3 +1,4 @@
+var state = require('./state');
 var mediator = require('./mediator');
 var routes = {
   vote: require('./routes/vote'),
@@ -11,21 +12,22 @@ page('/', function() {
   page.redirect('vote');
 });
 
+page('vote', routes.vote.start);
+
 page('vote/:pairing', function() {
   console.log("voting on stuff");
-  routes.vote.start();
+  // routes.vote.start();
 });
 
-page('about', function() {
-  routes.about.start();
-});
+page('about', routes.about.start);
 
-page('data', function() {
-  routes.data.start();
-});
+page('data', routes.data.start);
 
 mediator.subscribe("pair_updated", () => {
   console.log("heard");
+  console.log(state.get('pair'));
+  var pair = state.get('pair');
+  page('vote/' + pair[0].slug + "-vs-" + pair[1].slug);
 });
 
 page();
