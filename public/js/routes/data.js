65b7f09043a5ed7abbdcdb5db1287ea435ca4530
Ann Yuan <ayuan@thisisgrow.com>
2015-08-08 12:06:21 -0400

    shell of asking for vectors

--- public/js/routes/data.js
+++ public/js/routes/data.js
@@ -1,6 +1,15 @@
+var util = require('../util');
+var state = require('../state');
+var api = require('../api');
+var mediator = require('../mediator');
+
 module.exports = {
   initialize() {
 
   },
-  start() {}
+  start() {
+    var vectors = api.get('/vectors', (error, result) => {
+      console.log(result);
+    });
+  }
 };
